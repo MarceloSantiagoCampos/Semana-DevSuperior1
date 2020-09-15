5637c45ca3a1a64db45871854e01174a95b18680
Marcelo Santiago Campos <marcelosantiagocampos@gmail.com>
2020-09-15 15:12:22 -0300

    POST Record

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/controllers/RecordController.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dspesquisa.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dspesquisa.dto.RecordDTO;
+import com.devsuperior.dspesquisa.dto.RecordInsertDTO;
+import com.devsuperior.dspesquisa.services.RecordService;
+
+@RestController
+@RequestMapping(value = "/records")
+public class RecordController {
+
+	@Autowired
+	private RecordService recordService;
+	
+	@PostMapping
+	public ResponseEntity<RecordDTO> insert(@RequestBody RecordInsertDTO dto){
+		
+		RecordDTO newDTO = recordService.insert(dto);
+		
+		return ResponseEntity.ok().body(newDTO);		
+	}
+	
+	
+}
---
+++ backend/src/main/java/com/devsuperior/dspesquisa/services/RecordService.java
@@ -0,0 +1,48 @@
+package com.devsuperior.dspesquisa.services;
+
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dspesquisa.Repositories.GameRepository;
+import com.devsuperior.dspesquisa.Repositories.RecordRepository;
+import com.devsuperior.dspesquisa.dto.RecordDTO;
+import com.devsuperior.dspesquisa.dto.RecordInsertDTO;
+import com.devsuperior.dspesquisa.entities.Game;
+import com.devsuperior.dspesquisa.entities.Record;
+
+/**
+ * Essa clase faz a transferencia dos dados da Entidade para o DTO
+ * E envia o DTO para o controlador para ser utilizado para os webservices
+ * 
+ */
+
+@Service
+public class RecordService {
+
+	@Autowired
+	private RecordRepository recordRepository;
+	
+	@Autowired
+	private GameRepository gameRepository;
+	
+	@Transactional
+	public RecordDTO insert(RecordInsertDTO dto) {
+		Record entity = new Record();
+		entity.setName(dto.getName());
+		entity.setAge(dto.getAge());
+		entity.setMoment(Instant.now());
+		
+		Game game = gameRepository.getOne(dto.getGameId());
+		entity.setGame(game);
+		
+		entity = recordRepository.save(entity);
+		
+		return new RecordDTO(entity);
+		
+	}
+	
+	
+}
