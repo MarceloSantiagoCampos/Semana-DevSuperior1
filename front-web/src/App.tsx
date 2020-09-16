e6641ebe769d2573be65e62c29bc9f72970b0bbb
Marcelo Santiago Campos <marcelosantiagocampos@gmail.com>
2020-09-16 15:09:34 -0300

    Criando pagina Home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div>
       <Header />
+      <Home />
     </div>
   );
 }
