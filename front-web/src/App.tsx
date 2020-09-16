363d3a065fb47abd71e274c06efbcad3bb8df7f0
Marcelo Santiago Campos <marcelosantiagocampos@gmail.com>
2020-09-16 14:05:29 -0300

    Adicionando componente Header

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
-    <h1>Hello Semana DevSuperior!</h1>
+    <div>
+      <Header />
+    </div>
   );
 }
 
